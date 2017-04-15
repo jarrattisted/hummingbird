824132df21f3ed5fbd2799cad60c38d16246ffd5
jarrattisted <jarratt@helpdocs.io>
2017-04-15 11:33:29 +0100

    context menu

--- popup.js
+++ popup.js
@@ -1,12 +1,29 @@
-// Create one test item for each context type.
-var context = "selection";
-var url = "https://twitter.com/search?f=users&vertical=default&q=" + encodedName;
-var name = "";
-var encodedName = encodeURI(name);
+function constructTwitterUrl(name) {
+  // Basic structure for Twitter people search
+  var url = "https://twitter.com/search?f=users&vertical=default&q=";
+  if (name) {
+    // Encode the name so it works in the url
+    var encodedName = encodeURI(name);
+    // Return the url with name attached to the end
+    return url + encodedName;
+  } else(e) {
+   console.log("Nothing selected" + e);
+  }
+}
 
-chrome.contextMenus.create({}, function() {
-  
-});
+// A generic onclick callback function.
+function genericOnClick(info, tab) {
+  console.log("item " + info.menuItemId + " was clicked");
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+}
+
+// Create an item for right-click menu on Chrome
+chrome.contextMenus.create({
+                            "contexts": ["selection"],
+                            "onclick": constructTwitterUrl,
+                            "title": "Find '%s' on Twitter"
+                          });
 
 // Intentionally create an invalid item, to show off error checking in the
 // create callback.
