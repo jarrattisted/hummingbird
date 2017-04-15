a2545580eeb976e7a31e156b0dc3d635145c3f7a
jarrattisted <jarratt@helpdocs.io>
2017-04-15 13:12:09 +0100

    finish v1

--- hum.js
+++ hum.js
@@ -1,1 +1,14 @@
-chrome.contextMenus.create({"title": "Find '%s' on Twitter", "contexts":["selection"]});
+function findPersonOnTwitter(info) {
+  var url = "https://twitter.com/search?f=users&vertical=default&q=" +
+            info.selectionText;
+  chrome.tabs.create({
+    "url": url,
+  });
+}
+
+// Create right-click menu to open a new tab and use selection
+chrome.contextMenus.create({
+  "title": "Find '%s' on Twitter",
+  "contexts":["selection"],
+  "onclick": findPersonOnTwitter,
+});
